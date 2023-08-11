6298d5e5c2d7417d118852c108e1e20c024a5da5
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-08-11 20:57:11 -0300

    realice el llamado de imagenes a la pagina principal

--- src/Gimoji.jsx
+++ src/Gimoji.jsx
@@ -5,22 +5,30 @@ import Banner from "./components/Banner";
 import "./styles/style.css";
 import GiftCard from "./components/GiftCard";
 
-
 const apiKey = import.meta.env.VITE_APIKEY_GIPHY;
 const urlApi = import.meta.env.VITE_URL_API;
 
 export const Gimoji = () => {
   const [categories, setCategories] = useState([]);
+  const [gift, setGift] = useState([]);
 
   useEffect(() => {
     getCategories();
+    init();
   }, []);
 
   const getCategories = async () => {
     const resp = await fetch(`${urlApi}/v1/gifs/categories?api_key=${apiKey}`);
     const { data } = await resp.json();
-    console.log(data);
     setCategories(data);
+  };
+
+  const init = async () => {
+    const resp = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=xSXoVJnW1zvmU2hBqUWURZpYVzukXYfJ&q=animals&limit=24&offset=0&rating=g&lang=es&bundle=messaging_non_clips`
+    );
+    const { data } = await resp.json();
+    setGift(data);
   };
 
   return (
@@ -29,8 +37,8 @@ export const Gimoji = () => {
       <Banner></Banner>
       <Select categoriesdata={categories}></Select>
       <div className="album py-5 container">
-        <div>
-          <GiftCard></GiftCard>
+        <div className="row">
+          <GiftCard gift={gift}></GiftCard>
         </div>
       </div>
     </>
--- src/components/ui/SelectCategories.jsx
+++ src/components/ui/SelectCategories.jsx
@@ -3,11 +3,16 @@ import React from "react";
 const SelectCategories = ({ categoriesdata }) => {
   return (
     <div className="col-sm-4 pb-3">
-      <select className="form-select bg-black text-bg-dark" aria-label="Default select example">
+      <select
+        className="form-select bg-black text-bg-dark"
+        aria-label="Default select example"
+      >
         <option defaultValue={true}>Selecciona una Categoria</option>
 
         {categoriesdata.map((cate) => (
-          <option value={cate.gif.id}>{cate.name}</option>
+          <option key={cate.name} value={cate.gif.id}>
+            {cate.name}
+          </option>
         ))}
       </select>
     </div>
