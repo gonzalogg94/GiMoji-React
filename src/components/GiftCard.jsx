7cf0d207ae37e0830f8375dfafb1f8dd6229c38e
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-08-14 12:49:29 -0300

    Realice funcionalidad para el conteo de likes

--- src/components/GiftCard.jsx
+++ src/components/GiftCard.jsx
@@ -1,10 +1,47 @@
-const noImage = import.meta.env.VITE_NO_IMAGE;
+import { useEffect, useState } from "react";
+
+// const noImage = import.meta.env.VITE_NO_IMAGE;
 
 const GiftCard = ({ gift }) => {
+  const [likes, setLikes] = useState([]);
+ 
+
+  useEffect(() => {
+    console.log(likes);
+  }, [likes]);
+
+  const onClickLike = (giftId) => {
+    const gifExist = likes.find((item) => item.id == giftId);
+    if (!gifExist) {
+      const newLikesObj = {
+        id: giftId,
+        point: 1,
+      };
+      setLikes([...likes, newLikesObj]);
+    } else {
+      const updateLikes=likes.map((gif)=>{
+        if(gif.id===giftId){
+          return{
+            ...gif,
+            point: gif.point +1
+          }
+        }
+        return gif
+      })
+      setLikes(updateLikes)
+    }
+  };
+
+  const totalLikes=(gifId)=>{
+    const datalike= likes.find((item) => item.id===gifId)
+    return datalike ? datalike.point:0
+
+  }
+
   return (
     <>
       {gift.map((gift) => (
-        <div key={gift.id} className="col-lg-3 col-md-4 col-sm-1    pb-3">
+        <div key={gift.id} className="col-lg-3 col-md-4 col-sm-1 pb-3">
           <div>
             <div className="card shadow-sm">
               <img
@@ -13,10 +50,14 @@ const GiftCard = ({ gift }) => {
                 width={"100%"}
                 height={"230"}
               />
-              <div className="card-body">
+              <div className="card-body bg-dark">
                 <div className="d-flex justify-content-between align-items-center">
                   <div className="btn-group">
-                    <button type="button" className="btn btn-primary">
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      onClick={() => onClickLike(gift.id)}
+                    >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -27,7 +68,7 @@ const GiftCard = ({ gift }) => {
                       >
                         <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
                       </svg>
-                      <div>0</div>
+                      <div>{totalLikes(gift.id)}</div>
                     </button>
                     <button type="button" className="btn btn-sm btn-secondary">
                       <svg
