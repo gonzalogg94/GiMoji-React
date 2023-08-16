c97f1fe87e78d214da0ce6d8e3d60687573c1869
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2023-08-16 11:28:49 -0300

    cree custon hooks para almacenar logica

--- src/Gimoji.jsx
+++ src/Gimoji.jsx
@@ -4,9 +4,8 @@ import Select from "./components/ui/Select";
 import Banner from "./components/Banner";
 import "./styles/style.css";
 import GiftCard from "./components/GiftCard";
-import { giphyAxios } from "./config/AxiosGiphy";
 import useFetch from "./hooks/useFetch";
-
+import { Loading } from "./components/ui/Loading";
 
 const apiKey = import.meta.env.VITE_APIKEY_GIPHY;
 const urlApi = import.meta.env.VITE_URL_API;
@@ -16,42 +15,21 @@ export const Gimoji = () => {
   const [gift, setGift] = useState([]);
   const [serch, setSerch] = useState("monos");
 
-  const {data,isLoading} = useFetch(`${urlApi}gifs/categories?api_key=${apiKey}`);
-
+  const { data: dataCateg } = useFetch(
+    `${urlApi}gifs/categories?api_key=${apiKey}`
+  );
+  const { data: dataSearch, isLoading } = useFetch(
+    `${urlApi}gifs/search?api_key=${apiKey}&q=${serch}&limit=24&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+  );
 
   useEffect(() => {
-    getCategories();
-    init();
-  }, [data]);
-
-  useEffect(() => {
-    init();
-  }, [serch]);
-
-  // const getCategories = async () => {
-  //   const categData = await giphyAxios.get(`gifs/categories?api_key=${apiKey}`);
-  //   const { data } = categData;
-  //   setCategories(data.data);
-  // };
-  
-  const init = async () => {
-    const {data} = await  giphyAxios.get(
-      `${urlApi}gifs/search?api_key=${apiKey}&q=${serch}&limit=24&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
-    const { data:serchData } = data
-    setGift(serchData);
-  };
+    getCategories(dataCateg);
+    setGift(dataSearch);
+  }, [dataCateg, dataSearch]);
 
   const getCategories = async () => {
-   setCategories(data);
+    setCategories(dataCateg);
   };
-
-  // const init = async () => {
-  //   const resp = await fetch(
-  //     `${urlApi}gifs/search?api_key=${apiKey}&q=${serch}&limit=24&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
-  //   );
-  //   const { data } = await resp.json();
-  //   setGift(data);
-  // };
 
   const onchangebycategory = (e) => {
     setSerch(e.target.value);
@@ -62,6 +40,10 @@ export const Gimoji = () => {
       setSerch(e.target.value);
     }
   };
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
     <>
